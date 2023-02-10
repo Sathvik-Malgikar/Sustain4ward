e72fb5ffe8ef953a7766191971b17e892e4f1179
Sathvik-Malgikar <Sathvik.malgikar@gmail.com>
2023-02-10 12:22:46 +0530

    erh

--- backend/app.py
+++ backend/app.py
@@ -1,24 +1,36 @@
-from flask import Flask 
+from flask import Flask , send_file,request
 import flask_cors
 
 d=dict()
-d["heat"] ={ "val" : 7 ,"alt" : "https://sunearthinc.com/"  }
+d["heater"] ={ "val" : 7 ,"alt" : "https://sunearthinc.com/"  }
+d["plastic cutlery"] = { "val" : 6 ,"alt" : "https://ediblepro.com/"  }
+d["tissues"] = { "val" : 7 ,"alt" : "https://hankybook.com/eco-friendly-tissue-paper"  }
 d["petrol"] = 9
 d["gas"] = 5
-d["plastic"] = 6
 d["furniture"] = 4
 d["steel"] =3
-d["tissue"] = 7
 
 
 
 
 app=Flask(__name__)
 
-@app.route("/api/<category>")
-def home(category):
-    
+@app.route("/api/")
+def api():
+    category= request.args.get("product")
     return d[category]
+    pass
+
+@app.route("/")
+def home():
+    
+    return send_file("../frontend/index.html")
+    pass
+
+@app.route("/general/")
+def general():
+    
+    return send_file("../frontend/general.html")
     pass
 
 if __name__ == "__main__":
